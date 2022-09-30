e2c2fa7544dc4ed74aa5186d74fd5531c383566e
hggjb2021 <21016697753@qq.com>
2022-09-30 10:17:01 +0800

    New things

---
+++ A/Test.java
@@ -0,0 +1,8 @@
+package T.A;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+        AAA word = new AAA("for you");
+        System.out.println(word.getTranslate());
+    }
+}
